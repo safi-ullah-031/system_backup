6c422e300001411e9afa6262b820f7af8ab299d1
Safi Ullah <safiullah445@outlook.com>
2025-03-24 05:52:07 +0500

    Automation added
    It will automatically request for admin privileges

--- setup_task.py
+++ setup_task.py
@@ -1,12 +1,25 @@
 import os
+import sys
+import ctypes
 
-# Define the backup script location (Change this to the actual path)
+# Check if running as admin
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    # Relaunch script as Administrator
+    print("Requesting Administrator Privileges...")
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    sys.exit()
+
+# Define the backup script location
 backup_script = r"D:\system_backup\system_backup.py"
-
-# Define task name
 task_name = "Automated System Backup"
 
-# Define task creation command
+# Task Scheduler command
 command = f'''
 SCHTASKS /Create /TN "{task_name}" /TR "python {backup_script}" /SC DAILY /ST 02:00 /RL HIGHEST /F
 '''
